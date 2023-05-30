3b2a7b004e8587e11b3130c0a0b0c16c7d7e4e82
sandunrmst <sandunalugc@gmail.com>
2023-05-30 13:02:11 +0530

    Soluation 2

---
+++ getSum2.py
@@ -0,0 +1,15 @@
+
+import re
+
+file = open("regex_sum_1806427.txt", 'r')
+
+all_data = file.read()
+
+# This output all values as list separated comar
+line_values = re.findall('[0-9]+', all_data)
+
+sum = 0
+for value in line_values:
+    sum = sum + int(value)
+
+print(f"In file total of the number list is {sum}")
